186da6557c3e0cecb70bc8d0b4b0a503d188dcab
abipersonalinformationtech-max <abipersonalinformationtech@gmail.com>
2026-06-25 21:19:02 -0700

    Add files via upload

--- project/app.py
+++ project/app.py
@@ -1,35 +1,10 @@
-from flask import Flask, request, render_template
-import pymysql
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-conn = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="",   
-    database="jr_food_order"
-)
-
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/order', methods=['POST'])
-def order():
-    name = request.form['name']
-    phone = request.form['phone']
-    food = request.form['food']
-
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "INSERT INTO orders (customer_name, phone, food_item) VALUES (%s,%s,%s)",
-        (name, phone, food)
-    )
-
-    conn.commit()
-
-    return "Order Placed Successfully"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def index():
+	return render_template("index.html")
+	
+if __name__=='__main__':
+	app.run()
